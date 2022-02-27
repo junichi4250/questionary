83943c8eac47dc59b78d8f550f2ca565d1cac827
katojun <jkky0225@gmail.com>
2022-02-27 10:40:28 +0900

    add:scoreカラム

--- backend/resources/views/admin/index.blade.php
+++ backend/resources/views/admin/index.blade.php
@@ -6,33 +6,43 @@
 
 <div class="bg-gray-100 py-32 px-10 min-h-screen">
     <div class="p-10">
-        <p class="text-3xl text-center mb-8">アンケート管理システム</p>
+        <p class="text-3xl text-center mb-8">レビュー管理システム</p>
+        @include('admin.logout')
+        @include('admin.search')
         <span class="">全25件中　11~20件</span>
         <span class="float-right">|1|2|3</span>
         <table class="table-auto text-left">
             <thead>
                 <tr class="border-b-2">
-                    <th class="px-8 py-2">ID</th>
-                    <th class="px-24 py-2">氏名</th>
-                    <th class="px-16 py-2">性別</th>
-                    <th class="px-16 py-2">年代</th>
-                    <th class="pr-48 pl-36 py-2">内容</th>
+                    <th class="px-4 pr-24 py-2">ID</th>
+                    <th class="px-4 pr-32 py-2">氏名</th>
+                    <th class="px-4 pr-32 py-2">性別</th>
+                    <th class="px-4 pr-32 py-2">年代</th>
+                    <th class="px-4 pr-32 py-2">評価</th>
+                    <th class="px-4 pr-64 py-2">内容</th>
                 </tr>
             </thead>
             <tbody>
-                @foreach ($answers as $answer)
+                @if (!isset($reviews))
                 <tr>
-                    <td class="px-8 py-2">{{ $answer->id }}</td>
-                    <td class="px-24 py-2">{{ $answer->fullname }}</td>
-                    <td class="px-16 py-2">{{ $answer->gender }}</td>
-                    <td class="px-16 py-2">{{ $answer->age }}</td>
-                    <td class="pr-48 pl-36 py-2">{{ $answer->feedback }}</td>
-                    <td class="px-16 py-2">
+                    <td>レビューがありませんでした</td>
+                </tr>
+                @else
+                @foreach ($reviews as $review)
+                <tr>
+                    <td class="px-4 py-2">{{ $review->id }}</td>
+                    <td class="px-4 py-2">{{ $review->name }}</td>
+                    <td class="px-4 py-2">{{ $review->gender }}</td>
+                    <td class="px-4 py-2">{{ $review->age }}</td>
+                    <td class="px-4 py-2">{{ $review->score }}点</td>
+                    <td class="px-4 py-2">{{ $review->feedback }}</td>
+                    <td class="px-4 py-2">
                         <button class="py-2 px-5 bg-red-100 border border-black"
-                        onclick="location.href='{{ route('admin.show', ['id' => $answer->id]) }}'">詳細</button>
+                            onclick="location.href='{{ route('admin.show', ['id' => $review->id]) }}'">詳細</button>
                     </td>
                 </tr>
                 @endforeach
+                @endif
             </tbody>
         </table>
     </div>
--- backend/resources/views/admin/show.blade.php
+++ backend/resources/views/admin/show.blade.php
@@ -8,23 +8,23 @@
     <div class="p-10 md:w-3/4 lg:w-1/2 mx-auto">
         <p class="text-3xl text-center mb-8">アンケート管理システム</p>
 
-        <form method="POST" action="{{ route('admin.delete', ['id' => $answer->id]) }}">
+        <form method="POST" action="{{ route('admin.delete', ['id' => $review->id]) }}">
             <div class="flex items-center mb-5">
                 <label for="name" class="inline-block w-32 mr-8 text-left font-bold text-gray-600">ID</label>
-                <p class="flex-1 py-2 border-b-2 border-gray-400">{{ $answer['fullname'] }}</p>
+                <p class="flex-1 py-2 border-b-2 border-gray-400">{{ $review['name'] }}</p>
             </div>
 
             <div class="flex items-center mb-5">
                 <label for="name" class="inline-block w-32 mr-8 text-left font-bold text-gray-600">氏名</label>
-                <p class="flex-1 py-2 border-b-2 border-gray-400">{{ $answer['fullname'] }}</p>
+                <p class="flex-1 py-2 border-b-2 border-gray-400">{{ $review['name'] }}</p>
             </div>
             <div class="flex items-center mb-5">
                 <label for="number" class="inline-block w-32 mr-8 text-left 
                                      font-bold text-gray-600">性別</label>
                 <p class="flex-1 py-2 border-b-2 border-gray-400">
-                    @if($answer['gender'] == '0')
+                    @if($review['gender'] == '0')
                     男性
-                    @elseif($answer['gender'] == '1')
+                    @elseif($review['gender'] == '1')
                     女性
                     @endif
                 </p>
@@ -34,17 +34,17 @@
                 <label class="inline-block w-32 mr-8 text-left 
                                   font-bold text-gray-600">年代</label>
                 <p class="flex-1 py-2 border-b-2 border-gray-400">
-                    @if($answer['age_id'] == '0')
+                    @if($review['age_id'] == '0')
                     10代以下
-                    @elseif($answer['age_id'] == '1')
+                    @elseif($review['age_id'] == '1')
                     20代
-                    @elseif($answer['age_id'] == '2')
+                    @elseif($review['age_id'] == '2')
                     30代
-                    @elseif($answer['age_id'] == '3')
+                    @elseif($review['age_id'] == '3')
                     40代
-                    @elseif($answer['age_id'] == '4')
+                    @elseif($review['age_id'] == '4')
                     50代
-                    @elseif($answer['age_id'] == '5')
+                    @elseif($review['age_id'] == '5')
                     60代以上
                     @endif
                 </p>
@@ -54,14 +54,14 @@
             <div class="flex items-center mb-5">
                 <label class="inline-block w-32 mr-8 text-left 
                                      font-bold text-gray-600">メールアドレス</label>
-                <p class="flex-1 py-2 border-b-2 border-gray-400">{{ $answer['email'] }}</p>
+                <p class="flex-1 py-2 border-b-2 border-gray-400">{{ $review['email'] }}</p>
             </div>
 
             <div class="flex items-center mb-5">
                 <label class="inline-block w-32 mr-8 text-left 
                                      font-bold text-gray-600">メールマガジン送信可否</label>
                 <p class="flex-1 py-2 border-b-2 border-gray-400">
-                    @if($answer['is_send_email'] == '0')
+                    @if($review['is_send_email'] == '0')
                     送信不可
                     @else
                     送信許可
@@ -70,18 +70,23 @@
             </div>
 
             <div class="flex mb-5">
+                <label class="inline-block w-32 mr-8 text-left font-bold text-gray-600">評価</label>
+                <p class="flex-1 py-2 border-b-2 border-gray-400">{{ $review['score'] }}点</p>
+            </div>
+
+            <div class="flex mb-5">
                 <label class="inline-block w-32 mr-8 text-left font-bold text-gray-600">ご意見</label>
-                <p class="flex-1 py-2 border-b-2 border-gray-400">{{ $answer['feedback'] }}</p>
+                <p class="flex-1 py-2 border-b-2 border-gray-400">{{ $review['feedback'] }}</p>
             </div>
 
             <div class="flex mb-5">
                 <label class="inline-block w-32 mr-8 text-left font-bold text-gray-600">登録日時</label>
-                <p class="flex-1 py-2 border-b-2 border-gray-400">{{ $answer['created_at'] }}</p>
+                <p class="flex-1 py-2 border-b-2 border-gray-400">{{ $review['created_at'] }}</p>
             </div>
 
-            <div class="text-center">
-                <a href="{{ route('admin.index') }}" class="py-3 px-4 bg-blue-400 text-white font-bold">一覧に戻る</button>
-                <button class="py-3 px-8 bg-blue-400 text-white font-bold">削除</button>
+            <div class="text-center mt-16">
+                <a href="{{ route('admin.index') }}" class="py-3 px-4 mr-8 bg-blue-400 text-white font-bold">一覧に戻る</a>
+                <a href="{{ route('admin.index') }}" class="py-3 px-8 bg-blue-400 text-white font-bold">削除</a>
             </div>
         </form>
     </div>
