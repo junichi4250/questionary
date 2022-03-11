085584d25ada01b36e8f995fc7f70ebb42b437ec
katojun <jkky0225@gmail.com>
2022-03-11 21:43:44 +0900

    refs:shopを複数形に修正

--- backend/database/seeders/ShopsTableSeeder.php
+++ backend/database/seeders/ShopsTableSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class ShopTableSeeder extends Seeder
+class ShopsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
