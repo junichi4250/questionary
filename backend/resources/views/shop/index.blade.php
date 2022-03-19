b2c316dfc1026ca63e6cb0cef2f5401d69c5a839
katojun <jkky0225@gmail.com>
2022-03-19 15:46:03 +0900

    ref:お店一覧レイアウト調整

--- backend/resources/views/shop/index.blade.php
+++ backend/resources/views/shop/index.blade.php
@@ -7,15 +7,16 @@
 <div class="bg-gray-100 py-32 px-10 min-h-screen">
     <div class="p-10 md:w-3/4 mx-auto">
         <p class="text-3xl text-center mb-8">お店一覧</p>
+        @foreach ($shops as $shop)
         <div class="flex">
-            @foreach ($shops as $shop)
-            <img src="image/shop_1.jpeg" class="w-2/5 mx-8">
+            <img src="image/shop_1.jpeg" class="w-2/5 mx-8 mb-12 flex">
             <div class="mx-auto">
-                <p class="">{{ $shop->shop_name }}</p>
+                <p class="text-2xl mb-10">{{ $shop->shop_name }}</p>
+                <p class="mb-10">評価：★★★☆☆（3.0点）</p>
                 <a href="{{ route('review.index',  ['shop_id' => $shop->shop_id]) }}"
                     class="mt-8 pt-2 px-2 bg-blue-200 border-2">レビューする</a>
             </div>
-            @endforeach
         </div>
+        @endforeach
     </div>
 </div>
