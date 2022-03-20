c3eae5d1cb25aceaa3e602640b96d5c2492fe661
katojun <jkky0225@gmail.com>
2022-03-20 18:03:55 +0900

    fix:seed順変更

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
             AgesTableSeeder::class,
-            ReviewsTableSeeder::class,
             ShopsTableSeeder::class,
+            ReviewsTableSeeder::class,
             UsersTableSeeder::class,
         ]);
     }
