0201c457015f2972c6db4ce2494913b6d1f645c9
katojun <jkky0225@gmail.com>
2022-05-09 18:55:21 +0900

    refs:relation

--- backend/app/Models/Shop.php
+++ backend/app/Models/Shop.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\Review;
 
 class Shop extends Model
 {
@@ -12,4 +14,9 @@ class Shop extends Model
     protected $fillable = [
         'name',
     ];
+
+    public function review(): BelongsTo
+    {
+        return $this->belongsTo(Review::class);
+    }
 }
